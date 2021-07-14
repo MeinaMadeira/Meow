66a848a16884acdbe25c3c5ac666379133bd1d18
Akane Shoji <cahierreco+dev@gmail.com>
2021-07-14 09:50:55 +0900

    入力文字数に応じて猫の鳴き声を変える

--- prompt.js
+++ prompt.js
@@ -1,10 +1,31 @@
+const meowOfCat = [
+	"にゃーん",
+	"うなーお",
+	"なむなむ",
+	"カッカッカッ",
+	"フシャーッ",
+	"うるるるる…",
+	"ごあーん",
+	"ぎゃぅ",
+	"んみゃあ",
+	"んんー",
+	"ひぁー",
+	"みゃおう"
+];
+
+
 function Meowize() {
 	const textbox = document.getElementById("meow");
 	const inputValue = textbox.value;
-	const catweet = "https://twitter.com/intent/tweet?text=" + inputValue
+	const meowInt = inputValue.length % meowOfCat.length;
+	const outputValue = meowOfCat[meowInt];
+
+
+	const catweet = "https://twitter.com/intent/tweet?text=" + "🐈 < " + outputValue + "&hashtags=社会性フィルター";
 	
-	document.getElementById("postmeow").innerHTML = "monimoni";
+	document.getElementById("postmeow").innerHTML = outputValue;
 	document.getElementById("meowlink").href = catweet;
+	document.getElementById("meow").value = outputValue;
 }
 
 document.getElementById("meowButton").onclick = function () {
